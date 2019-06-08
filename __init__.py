860497f8246ef084ec5909503a40b65e05bfc40d
Aditya Walvekar <aditya@primemindz.com>
2019-06-09 03:19:24 +0530

    Added Volume Mounting

--- __init__.py
+++ __init__.py
@@ -51,7 +51,7 @@ def index():
                 print(e1)
         else:
             return {'status':False, "desc":"Env Name not a valid env URL"}
-        container = client.containers.run(params['image_name'], detach = True, ports = {str(port)+"/tcp":params['port']}, volumes= {str(path):{'bind': '/home/nvie', 'mode': 'rw'}})
+        container = client.containers.run(params['image_name'], detach = True, ports = {str(params['port'])+"/tcp":port}, volumes= {str(path):{'bind': '/home/nvie', 'mode': 'rw'}})
         mapping = ContainerPortMapping(env_name = env_name, container = container.id, port = port, old_port = old_port)
         db.session.add(mapping)
         conf = '''server {
@@ -67,6 +67,7 @@ def index():
             file.write(conf)
         subprocess.call(["sudo", "service", "nginx", "restart"])
         db.session.commit()
+        print("Created Container")
     except Exception as e:
         print(e)
         return {'status':False}
